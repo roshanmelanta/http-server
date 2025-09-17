5b0983e18600f94bb254b91e539bfe766be28783
roshanmelanta <rmelanta255@gmail.com>
2025-09-18 00:30:05 +0530

    add core request and response types

---
+++ src/main/java/com/server/pojos/HttpRequest.java
@@ -0,0 +1,57 @@
+package com.server.pojos;
+
+import com.server.contract.HttpMethod;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Map;
+
+public class HttpRequest {
+    private final HttpMethod httpMethod;
+    private final URI uri;
+    private final Map<String, List<String>> requestHeaders;
+
+    private HttpRequest(HttpMethod httpMethod, URI uri, Map<String, List<String>> requestHeaders) {
+        this.httpMethod = httpMethod;
+        this.uri = uri;
+        this.requestHeaders = requestHeaders;
+    }
+
+    public URI getUri() {
+        return uri;
+    }
+
+    public HttpMethod getHttpMethod() {
+        return httpMethod;
+    }
+
+    public Map<String, List<String>> getRequestHeaders() {
+        return requestHeaders;
+    }
+
+    public static class Builder {
+        private HttpMethod httpMethod;
+        private URI uri;
+        private Map<String, List<String>> requestHeaders;
+
+        public Builder() {
+
+        }
+
+        public void setHttpMethod(HttpMethod httpMethod) {
+            this.httpMethod = httpMethod;
+        }
+
+        public void setRequestHeaders(Map<String, List<String>> requestHeaders) {
+            this.requestHeaders = requestHeaders;
+        }
+
+        public void setUri(URI uri) {
+            this.uri = uri;
+        }
+
+        public HttpRequest build() {
+            return new HttpRequest(httpMethod, uri, requestHeaders);
+        }
+    }
+}
---
+++ src/main/java/com/server/pojos/HttpStatusCode.java
@@ -0,0 +1,15 @@
+package com.server.pojos;
+
+import java.util.Map;
+
+/**
+ * Map of status code values and meanings.
+ */
+public class HttpStatusCode {
+    public static final Map<Integer, String> STATUS_CODES = Map.of(
+            200, "OK",
+            400, "BAD_REQUEST",
+            404, "NOT_FOUND",
+            500, "INTERNAL_SERVER_ERROR"
+    );
+}
